367089de2a611c17e95445704859105cfedcf91a
nekattrin <negreeva.katerina@gmail.com>
2024-03-17 13:21:51 +0300

    final gui

--- ToDo_App/gui.py
+++ ToDo_App/gui.py
@@ -1,18 +1,31 @@
 import functions
 import PySimpleGUI as sg
 import time
+import os
 
 sg.theme('DarkPurple')
 
+if not os.path.exists('todos.txt'):
+    with open('todos.txt', 'w') as file:
+        pass
+
+if os.path.exists('addd.png'):
+    add_button = sg.Button(key='Add', image_source='addd.png',
+                           mouseover_colors='DeepPink')
+else:
+    add_button = sg.Button("Add", key='Add',
+                           mouseover_colors='DeepPink')
+
+
 clock = sg.Text('', key='clock')
 label = sg.Text("Type in a to-do")
-inputBox = sg.InputText(tooltip="Enter a to-do", key="todo")
-add_button = sg.Button("Add")
+inputBox = sg.InputText(tooltip="Enter a to-do", key="todo", size=46)
+
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=[45, 10])
-edit_button = sg.Button("Edit")
-complete_button = sg.Button("Complete")
-exit_button = sg.Button("Exit")
+edit_button = sg.Button("Edit", mouseover_colors='DeepPink')
+complete_button = sg.Button("Complete", mouseover_colors='DeepPink')
+exit_button = sg.Button("Exit", mouseover_colors='DeepPink')
 
 window = sg.Window('To-Do App',
                    layout=[[clock],
@@ -29,11 +42,12 @@ while True:
     print(value)
     match event:
         case "Add":
-            todos = functions.get_todos()
-            new_todo = value['todo'] + "\n"
-            todos.append(new_todo)
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
+            if value['todo'] != '':
+                todos = functions.get_todos()
+                new_todo = value['todo'] + "\n"
+                todos.append(new_todo)
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
 
         case "Edit":
             try:
